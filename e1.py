9edfa55a22de4d75338e52a7c66d94138985c7e0
Your Name <arodriguez20042008@gmail.com>
2024-10-07 13:42:07 +0200

    cambio

--- e1.py
+++ e1.py
@@ -2,4 +2,17 @@ mi_nombre="Adrian"
 mi_edad="19"
 print("mi nombre es", mi_nombre)
 print ("mi edad es" , mi_edad)
-print ("pruebsdfASFnANFOKFnkOJFN")
+
+lado=5 
+area= lado*lado
+print ("el area del cuadradoes", area)
+
+edad=16
+es_adulto= edad>=18
+print ("es adulto?", es_adulto)
+
+edad= input("introduce tu edad ")
+edad =int (edad)
+edad_futura = edad + 10
+print ("en 10 años tendras", edad_futura, "años")
+
