9baaec98460c9b810790e4e68acb02f313500115
adrirf7 <arodriguez20042008@gmail.com>
2024-10-28 14:22:17 +0100

    s

---
+++ Ejercicios/Ejercicios_9/elevar_cuadrado.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+array= np.array([6,7,8,9,10])
+
+cuadrado= array **2
+
+print(cuadrado)
---
+++ Ejercicios/Ejercicios_9/filtrar_mayores.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+array= np.array([2,4,8,9,10,12,7,3])
+
+mayores_5= array[array>5] 
+print(mayores_5)
+
